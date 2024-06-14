e553f1ce7b1742c734023c2d316286b7e6c6fd6f
xanderjensen <xanderjamesjensen@icloud.com>
2024-06-13 19:16:23 -0600

    Ready to Publish

---
+++ index.js
@@ -0,0 +1,38 @@
+const Stripe = require("stripe")
+const bodyParser = require("body-parser")
+const express = require('express');
+require('dotenv').config()
+const app = express();
+
+const stripe = Stripe(process.env.STRIPE_SECRET_TEST)
+
+const cors = require("cors");
+
+app.use(bodyParser.json());
+app.use(express.static("."));
+app.use(cors());
+
+app.post('/create-intent', cors(), async (req, res, next) => {
+    const model = req.body;
+    console.log(process.env.STRIPE_SECRET_TEST)
+
+    try {
+        const intent = await stripe.paymentIntents.create({
+            amount: model.amount,
+            currency: 'usd',
+            automatic_payment_methods: {
+                enabled: true,
+            },
+            receipt_email: model.receipt_email,
+            description: model.description
+        });
+    
+        res.json(intent);
+    } catch (e) {
+        res.status(500).send({})
+    }
+});
+
+app.listen(process.env.PORT || 4000, () => {
+    console.log("Server is listening on port 4000")
+})
